92682c75ad74fe4419188f28bf23b53fbb76b540
Mx <1318892153@qq.com>
2022-03-02 15:57:01 +0800

    1

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ var server = http.createServer(function(request, response){
                <link rel="stylesheet" href="/x">
             </head>
             <body>
-            <h1>你好</h1>
+            <h1>你好hello</h1>
             <script src="/y"> </script>
             </body>
     `)
